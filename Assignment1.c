16fd1e0918cb5aaacf5fde99a248c3f9f67738bb
Diwash Bhattarai <diwashbhattarai86@gmail.com>
2021-07-24 16:30:36 +0545

    file added

---
+++ Assignment1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int sum(int a, int b);
+int product(int a, int b);
+
+int main(){
+<<<<<<< HEAD
+    int a,b;
+=======
+    int c,d;
+>>>>>>> product
+
+    return 0;
+}
+
+
+int product(int a,int b){
+    return a*b;
+}
